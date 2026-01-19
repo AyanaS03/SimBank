fcdd92af48255c82c1a37afd564730c8172bc95d
Ayana <satylganovaaiana@gmail.com>
2026-01-19 22:04:27 +0600

    Resolved conflict

---
+++ src/kg/sim_bank/delivery/Delivery.java
@@ -0,0 +1,9 @@
+package kg.sim_bank.delivery;
+
+public class Delivery {
+    static void main() {
+
+        System.out.println("Delivery Yandex");
+        System.out.println("Delivery Glovo");
+    }
+}
